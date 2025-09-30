796a1c55c2cdf0cbd5033359394126cac3801cba
simo680 <simozoid670@duck.com>
2025-09-30 16:15:01 +0300

    Create Input component and edit styles variables

--- frontend/src/components/ui/Input/Input.tsx
+++ frontend/src/components/ui/Input/Input.tsx
@@ -1,7 +1,41 @@
-const Input = () => {
-  return (
-    <div>Input</div>
-  )
-}
+import React from "react";
+import clsx from "clsx";
 
-export default Input
+import type { InputProps } from "./type";
+
+import s from "./Input.module.scss";
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  (
+    {
+      label,
+      className,
+      name,
+      onChange,
+      onBlur,
+      appearance = "primary",
+      type,
+      placeholder,
+      ...props
+    },
+    ref
+  ) => {
+    return (
+      <>
+        {label && <label htmlFor={name}>{label}</label>}
+        <input
+          className={clsx(s.input, s[appearance], className)}
+          name={name}
+          type={type}
+          placeholder={placeholder}
+          onChange={onChange}
+          onBlur={onBlur}
+          ref={ref}
+          {...props}
+        />
+      </>
+    );
+  }
+);
+
+export default Input;
---
+++ frontend/src/components/ui/Input/type.ts
@@ -0,0 +1,6 @@
+import type React from "react";
+
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
+  label?: string;
+  appearance?: "primary" | "secondary";
+};
