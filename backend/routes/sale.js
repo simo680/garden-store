0f52e1dd614b4c4efc7fb62094018f82b25191a4
simo680 <simozoid670@duck.com>
2025-10-02 18:27:28 +0300

    Format code

--- backend/routes/sale.js
+++ backend/routes/sale.js
@@ -1,18 +1,14 @@
-const { request } = require('express');
-const express = require('express');
+const { request } = require("express");
+const express = require("express");
 
 const router = express.Router();
 
+router.get("/send", (req, res) => {
+  res.json({});
+});
 
-router.get('/send', (req, res) =>{
-    res.json({});
-
-})
-
-router.post('/send', (req, res) => {
-    
-    res.json({status: 'OK', message: 'request processed'})
-})
-
+router.post("/send", (req, res) => {
+  res.json({ status: "OK", message: "request processed" });
+});
 
 module.exports = router;
