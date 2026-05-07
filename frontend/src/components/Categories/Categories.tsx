63f2cc69c6d282f966b01a3486e2996c5493dc5e
simo680 <simozoid670@duck.com>
2026-05-07 20:45:17 +0300

    some fixes

--- frontend/src/components/Categories/Categories.tsx
+++ frontend/src/components/Categories/Categories.tsx
@@ -3,6 +3,7 @@ import { LineButton } from '../ui/LineButton/LineButton';
 
 import s from './Categories.module.scss';
 
+const Categories = () => {
   return (
     <div className={s.categories}>
       <LineButton
